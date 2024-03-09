439524aa63127a4aaa53c826d6864e77e6c6129c
Joseph Sypal <60202320+jsypal04@users.noreply.github.com>
2024-03-08 20:44:55 -0500

    Create the ast.h header and add classes for expression nodes.

---
+++ src/ast.h
@@ -0,0 +1,95 @@
+#ifndef AST_H
+#define AST_H
+
+#include <string>
+#include <memory>
+#include "tokens.h"
+
+// base class for all statements
+class StmtAST {
+public:
+    virtual ~StmtAST() = default;
+};
+
+// class for assignment nodes
+class AssignStmtAST : StmtAST {
+private:
+    std::string varName;
+    Token type;
+    std::unique_ptr<StmtAST> RHS;
+public:
+    AssignStmtAST(std::string name, Token typ, std::unique_ptr<StmtAST> rhs) {
+        varName = name;
+        type = typ;
+        RHS = std::move(rhs); // no idea why we need to use std::move() but it doesn't work otherwise
+    }
+};
+
+// class for arithmetic expression nodes
+class ExprStmtAST : StmtAST {
+private:
+    std::unique_ptr<StmtAST> term;
+    std::unique_ptr<StmtAST> expr_p;
+public:
+    ExprStmtAST(std::unique_ptr<StmtAST> trm, std::unique_ptr<StmtAST> exp) {
+        term = std::move(trm);
+        expr_p = std::move(exp);
+    }
+};
+
+// class for the expr_p production rule
+class Expr_PStmtAST : StmtAST {
+private:
+    Operator op;
+    std::unique_ptr<StmtAST> term;
+    std::unique_ptr<StmtAST> expr_p;
+public:
+    Expr_PStmtAST(Operator o, std::unique_ptr<StmtAST> trm, std::unique_ptr<StmtAST> exp) {
+        op = o;
+        term = std::move(trm);
+        expr_p = std::move(exp);
+    }
+};
+
+class TermStmtAST : StmtAST {
+private:
+    std::unique_ptr<StmtAST> factor;
+    std::unique_ptr<StmtAST> term_p;
+public:
+    TermStmtAST(std::unique_ptr<StmtAST> fctr, std::unique_ptr<StmtAST> trm_p) {
+        factor = std::move(fctr);
+        term_p = std::move(trm_p);
+    }
+};
+
+class Term_PStmtAST : StmtAST {
+private:
+    Operator op;
+    std::unique_ptr<StmtAST> factor;
+    std::unique_ptr<StmtAST> term_p;
+public:
+    Term_PStmtAST(Operator o, std::unique_ptr<StmtAST> fctr, std::unique_ptr<StmtAST> trm_p) {
+        op = o;
+        factor = std::move(fctr);
+        term_p = std::move(trm_p);
+    }
+};
+
+class FactorStmtAST : StmtAST {
+private:
+    // might change how lparen and rparen are implemented
+    bool lparen;
+    std::unique_ptr<StmtAST> object;
+    bool rparen;
+public:
+    // only input tokens for lparen and rparen if obj is an ExprStmtAST, othermise input null
+    FactorStmtAST(bool lp, bool rp, std::unique_ptr<StmtAST> obj) {
+        lparen = lp;
+        rparen = rp;
+        object = std::move(obj);
+    }
+};
+
+
+
+#endif
