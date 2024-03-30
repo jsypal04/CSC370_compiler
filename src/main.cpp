2cb919fa543fb9bc916d5216489f2c6093384231
Joseph Sypal <60202320+jsypal04@users.noreply.github.com>
2024-03-29 23:40:45 -0400

    Connect the semantic analyzer to the output of the parser.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <typeinfo>
 #include "headers/parser.h"
+#include "headers/semantics.h"
 
 int main(int argc, char* argv[]) {
     char* path = argv[1];
     
     Parser parser(path);
+    Semantics semantic_analyzer;
 
     if (parser.lexer->fileNotFound) {
         std::cout << "File \"" << path << "\" not found.\n";
@@ -13,6 +15,7 @@ int main(int argc, char* argv[]) {
     }
 
     std::unique_ptr<ProgStmtAST> program = std::move(parser.parse());
+    semantic_analyzer.traverse_prog(std::move(program));
 
     parser.destroyLexer();
 
